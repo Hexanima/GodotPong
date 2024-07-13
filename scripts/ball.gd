46f16f9134ce961365e96aaed5766938d0ed6646
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-07-13 10:25:08 -0300

    Initial structure

---
+++ scripts/ball.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+@export var SPEED := 150.0
+
+var x_direction := -1
+var y_direction := -1
+
+func _ready():
+	contact_monitor = true
+	max_contacts_reported = 2
+
+func _physics_process(delta):
+	linear_velocity = Vector2(x_direction * SPEED,y_direction * SPEED)
+	
+	pass
+	
+func change_direction():
+	x_direction = -x_direction
+
+func _on_body_entered(body):
+	if body is Player:
+		change_direction()
+	if body is StaticBody2D:
+		y_direction = -y_direction
---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+class_name Player
+
+@export var SPEED = 300.0
+
+@export var UP : StringName = "p1_up"
+@export var DOWN : StringName = "p1_down"
+
+func _physics_process(delta):
+	var direction = Input.get_axis(UP, DOWN)
+	
+	velocity.y = direction * SPEED
+	
+	move_and_slide()
