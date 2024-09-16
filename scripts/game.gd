c898d21fe2059669788fde7db3aa99c0f2878bb5
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-09-16 18:43:15 -0300

    Added Score UI

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,6 +4,8 @@ extends Node2D
 var newBall = preload("res://scenes/ball.tscn")
 @onready var player_1 = $Player1
 @onready var player_2 = $Player2
+@onready var score_1 = $Score1
+@onready var score_2 = $Score2
 
 var _p1_in_pos
 var _p2_in_pos
@@ -11,6 +13,8 @@ var _p2_in_pos
 func _ready():
 	_p1_in_pos = player_1.position.x
 	_p2_in_pos = player_2.position.x
+	score_1.text = str(player_1.score)
+	score_2.text = str(player_2.score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func reset_ball():
@@ -29,13 +33,13 @@ func _process(delta):
 		player_2.position.x = _p2_in_pos
 	if Input.is_action_just_pressed("reset"):
 		reset_ball()
-		display_scores()
-
 
 func _on_goal_1_on_reset():
 	player_1.score += 1
+	score_1.text = str(player_1.score)
 	reset_ball()
 
 func _on_goal_2_on_reset():
 	player_2.score += 1
+	score_2.text = str(player_2.score)
 	reset_ball()
