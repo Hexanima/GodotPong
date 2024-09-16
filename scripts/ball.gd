224d00c2d4881f3409605734c1318a1c928adc4b
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-09-16 18:35:10 -0300

    Added random directions

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,24 +2,26 @@ extends RigidBody2D
 
 class_name Ball
 
-@export var SPEED := 150.0
+@export var SPEED := 300.0
 
-var x_direction := -1
-var y_direction := -1
+var direction : Vector2 = Vector2(randf_range(-1.5,1.5),randf_range(-1.5,1.5)).normalized()
 
 func _ready():
 	contact_monitor = true
 	max_contacts_reported = 2
 
 func _physics_process(delta):
-	linear_velocity = Vector2(x_direction * SPEED,y_direction * SPEED)
+	linear_velocity = direction * SPEED
 	pass
 	
 func change_direction():
-	x_direction = -x_direction
+	if direction.x < 0:
+		direction = Vector2(randf_range(0.5,1.5),randf_range(0.5,1.5)).normalized()
+	elif direction.x > 0:
+		direction = Vector2(randf_range(-0.5,-1.5),randf_range(-0.5,-1.5)).normalized()
 
 func _on_body_entered(body):
 	if body is Player:
 		change_direction()
 	if body is StaticBody2D:
-		y_direction = -y_direction
+		direction = Vector2(direction.x, 0 - direction.y)
